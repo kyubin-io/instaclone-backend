41cc9b635cc6fe0a2f1c83e47c65d26afb07c3cf
김규빈 <inkblot626@gmail.com>
2025-01-14 18:10:35 +0900

    setup apollo server

---
+++ server.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+
+  type Book {
+    title: String
+    author: String
+  }
+
+    type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => "Hello",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
