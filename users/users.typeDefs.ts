ba221a31e8d984d2ca12872761f41163c4def175
김규빈 <inkblot626@gmail.com>
2025-01-23 00:04:10 +0900

    add createdAt, updatedAt, and syncronise

---
+++ users/users.typeDefs.ts
@@ -0,0 +1,11 @@
+const typeDefs = `#graphql
+  type User {
+    id: String!
+    firstName: String!
+    lastName: String
+    username: String
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
