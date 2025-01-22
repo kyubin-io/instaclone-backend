b644367816d3e925e3c7b4a8693028209fbe76f9
김규빈 <inkblot626@gmail.com>
2025-01-22 23:01:00 +0900

    delete prisma folder

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import "dotenv/config";
+import schema from "./schema";
 
 const typeDefs = `#graphql
   type Book {
