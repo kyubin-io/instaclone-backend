71c3be8e61d2767f3293f0524264d4d5dbee15e3
김규빈 <inkblot626@gmail.com>
2025-01-26 20:16:56 +0900

    graphql-tools/schema

--- schema.ts
+++ schema.ts
@@ -1,6 +1,6 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const loadedTypes = loadFilesSync(`$(__dirname)/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`$(__dirname)/**/*.queries,mutation.js`);
