ed78a88aa376d027f6d12e9b737eb0d5b4f3c725
김규빈 <inkblot626@gmail.com>
2025-02-01 23:05:07 +0900

    add users

---
+++ src/users/users.mutations.ts
@@ -0,0 +1,24 @@
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      const existingUser = await client.user.findFirst({
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+    },
+  },
+};
